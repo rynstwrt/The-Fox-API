2a670be9b6384f43293f141f686ab52496b28965
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-29 19:13:43 -0500

    Init

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Scraper = require("images-scraper");
+
+
+const NUM_IMAGES = 200;
+const PORT = process.env.PORT || 3000;
+const google = new Scraper({
+    puppeteer: {
+        headless: true,
+    }
+});
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+let results;
+
+
+app.get("/", (req, res) =>
+{
+    res.send("Please select an endpoint to use instead.");
+});
+
+
+app.get("/getafox", (req, res) =>
+{
+    if (!results)
+    {
+        res.json({status: "WAIT", message: "Error: Images not loaded yet. Try again in a few seconds."});
+        return;
+    }
+
+    const index = Math.floor(Math.random() * NUM_IMAGES);
+    res.json({status: "OK", url: results[index]});
+});
+
+
+app.listen(PORT, async () =>
+{
+    console.log("LISTENING ON PORT " + PORT)
+
+    results = await google.scrape("fox", NUM_IMAGES);
+    results = results.map(r => r["url"]);
+});
